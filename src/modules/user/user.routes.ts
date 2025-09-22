0d24b07b48b945c858247c0994dbe12933cbbe40
Ishan Hettiarachchi <ishananaguru@gmail.com>
2025-09-22 14:45:17 +0530

    refactor: remove unused user favorite food controllers, services, and routes; simplify user profile handling

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,18 +1,8 @@
 import express from 'express';
-import {
-    addUserFavoriteFoodController,
-    getUserFavoriteFoodsController,
-    getUserProfileController,
-    isFoodFavoriteController,
-    removeUserFavoriteFoodController,
-} from './user.controller';
+import { getUserProfileController } from './user.controller';
 
 const userRouter = express.Router();
 
 userRouter.get('/profile', getUserProfileController);
-userRouter.get('/favorites', getUserFavoriteFoodsController);
-userRouter.post('/favorites', addUserFavoriteFoodController);
-userRouter.post('/favorites/remove', removeUserFavoriteFoodController);
-userRouter.post('/favorites/is-favorite', isFoodFavoriteController);
 
 export default userRouter;
